c419c9b6001f99cec9c2a9af7b9f2dc0c9be1e23
nowantny <32875116+nowantny@users.noreply.github.com>
2021-08-09 16:11:31 -0400

    Add files via upload

---
+++ zaero/tables/clientfields.h
@@ -0,0 +1,15 @@
+/*
+ * =======================================================================
+ *
+ * Fields of the client to be saved.
+ *
+ * =======================================================================
+ */
+
+{"pers.weapon", CLOFS(pers.weapon), F_ITEM},
+{"pers.lastweapon", CLOFS(pers.lastweapon), F_ITEM},
+{"pers.lastweapon2", CLOFS(pers.lastweapon2), F_ITEM}, /* FS: Zaero specific */
+{"newweapon", CLOFS(newweapon), F_ITEM},
+{"", CLOFS(zCameraTrack), F_EDICT}, /* FS: Zaero specific */
+{"", CLOFS(zCameraLocalEntity), F_EDICT}, /* FS: Zaero specific */
+{NULL, 0, F_INT}
